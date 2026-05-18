8c559a8e053d276c159dc4f02eb77ae99f5d2f29
Maica <vitriolomaica@gmail.com>
2026-05-19 01:27:50 +0800

    index.php debugging

--- WS03/public/index.php
+++ WS03/public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -6,13 +7,12 @@ require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 
 use Framework\Router;
-// require '../helpers.php';
-// require basePath('Framework/Router.php');
-// require basePath('Framework/Database.php');
+
+// $db = new Database($config);
 
 $router = new Router();
 
-require basePath('routes.php');
+$routes = require basePath('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
